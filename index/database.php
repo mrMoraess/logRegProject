731aab8fc77e29b8a86130f4b2bc1ecbf87dd92e
mrMoraess <matheusrodriguesde347@gmail.com>
2024-01-12 16:48:59 -0300

    organização das variáveis de ambiente

--- index/database.php
+++ index/database.php
@@ -1,8 +1,15 @@
 <?php
-    $db_server = "localhost";
-    $db_user = "root";
-    $db_pass = "";
-    $db_name = "myWebApp1";
+    require_once __DIR__ ."/vendor/autoload.php";
+
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+
+    echo $_ENV["DB_PASS"];
+
+    $db_server = $_ENV["DB_SERVER"];
+    $db_user = $_ENV["DB_USER"];
+    $db_pass = $_ENV["DB_PASS"];
+    $db_name = $_ENV["DB_NAME"];
     $conn = "";
 
     $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
